88f17f3fed0f6ef8a8c64e6b687133734ae62c57
Tsvetanmarkov <markowcvetan@gmail.com>
2025-05-26 11:53:53 +0300

    remove some code  in nav link

--- Calendar/src/components/Nav/Nav.jsx
+++ Calendar/src/components/Nav/Nav.jsx
@@ -1,14 +1,8 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import Authentication from "../Account/Authentication";
 import "./Nav.css";
 
 function Nav() {
-  return (
-    <nav>
-      <NavLink to="/account/authentication"></NavLink>
-    </nav>
-  );
+  return <nav></nav>;
 }
 
 export default Nav;
