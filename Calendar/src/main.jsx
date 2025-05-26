06e1d9fd9d75baead19a9e30bffbe3c9b4d7907e
Tsvetanmarkov <markowcvetan@gmail.com>
2025-05-26 12:23:23 +0300

    setting chakra for project add some basic dark theme as default

--- Calendar/src/main.jsx
+++ Calendar/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "./components/ui/provider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
