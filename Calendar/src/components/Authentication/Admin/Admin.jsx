fab8f414e62b97212f840d16db60057646b55f66
Tsvetanmarkov <markowcvetan@gmail.com>
2025-06-02 10:04:01 +0300

    add some stylesa to the admin panel

--- Calendar/src/components/Authentication/Admin/Admin.jsx
+++ Calendar/src/components/Authentication/Admin/Admin.jsx
@@ -3,15 +3,14 @@ import { Navigate } from "react-router-dom";
 import { AuthContext } from "../AuthContext";
 import styles from "./Admin.module.css";
 
-
 function Admin() {
   const { isLoggedIn, user } = useContext(AuthContext);
   const [search, setSearch] = useState("");
   const [allUsers, setAllUsers] = useState([]);
   const [searchEvents, setSearchEvents] = useState([]);
-  const [findEvents, setFindEvents] = useState('');
+  const [findEvents, setFindEvents] = useState("");
   const [editingEventId, setEditingEventId] = useState(null);
-  const [eventData, setEventData] = useState({title: "", description: ""});
+  const [eventData, setEventData] = useState({ title: "", description: "" });
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -34,7 +33,7 @@ function Admin() {
     };
 
     fetchEvents();
-  },[]);
+  }, []);
 
   useEffect(() => {
     fetch("http://localhost:5000/api/auth/users", {
@@ -56,7 +55,9 @@ function Admin() {
   );
 
   const toggleBlock = async (id, block) => {
-    const endpoint = `http://localhost:5000/api/auth/${block ? "block" : "unblock"}/${id}`;
+    const endpoint = `http://localhost:5000/api/auth/${
+      block ? "block" : "unblock"
+    }/${id}`;
     const res = await fetch(endpoint, {
       method: "POST",
       headers: {
@@ -91,49 +92,52 @@ function Admin() {
       event.title.toLowerCase().includes(findEvents.toLowerCase()) ||
       event.description.toLowerCase().includes(findEvents.toLowerCase())
     );
-  })
+  });
 
   const deleteEvent = async (id) => {
-    try{
-    const res = await fetch(`http://localhost:5000/api/events/${id}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-    });
-    if (res.ok) {
-      setSearchEvents((events) => events.filter((e) => e._id !== id));
-    } else {
-      const error = await res.json();
-      console.error("Failed to delete event:", error.message);
-      alert("Failed to delete event: " + error.message);
-    }
-  } catch (error) {
+    try {
+      const res = await fetch(`http://localhost:5000/api/events/${id}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      if (res.ok) {
+        setSearchEvents((events) => events.filter((e) => e._id !== id));
+      } else {
+        const error = await res.json();
+        console.error("Failed to delete event:", error.message);
+        alert("Failed to delete event: " + error.message);
+      }
+    } catch (error) {
       console.error("Error deleting event:", error);
       alert("An error occurred while deleting the event.");
     }
-  }
+  };
 
   const startEditingEvent = (event) => {
     setEditingEventId(event._id);
-    setEventData({title: event.title, description: event.description});
-  }
+    setEventData({ title: event.title, description: event.description });
+  };
 
   const cancelEditing = () => {
     setEditingEventId(null);
-    setEventData({title: "", description: ""});
-  }
+    setEventData({ title: "", description: "" });
+  };
 
   const saveEdit = async () => {
     try {
-      const res = await fetch(`http://localhost:5000/api/events/${editingEventId}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-        body: JSON.stringify(eventData),
-      });
+      const res = await fetch(
+        `http://localhost:5000/api/events/${editingEventId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          body: JSON.stringify(eventData),
+        }
+      );
       if (res.ok) {
         const updatedEvent = await res.json();
         setSearchEvents((events) =>
@@ -156,108 +160,119 @@ function Admin() {
   return (
     <div className={styles.adminContainer}>
       <h2 className={styles.adminTitle}>Administration Hub</h2>
-  
-  
-      <section className={styles.panel}>
-        <h3 className={styles.panelTitle}>Users</h3>
-        <input
-          type="text"
-          placeholder="Search users"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          className={styles.searchInput}
-        />
-  
-        <ul className={styles.userList}>
-          {filteredUsers.map((u) => (
-            <li key={u._id} className={styles.userListItem}>
-              <div className={styles.userInfo}>
-                <span className={styles.userName}>{u.firstName}</span>
-                <span className={styles.userEmail}>({u.email})</span>
-              </div>
-  
-              <div className={styles.userAction}>
-                <button
-                  className={u.isBlocked ? styles.btnUnblock : styles.btnBlock}
-                  onClick={() => toggleBlock(u._id, !u.isBlocked)}
-                >
-                  {u.isBlocked ? "Unblock" : "Block"}
-                </button>
-                <button
-                  className={styles.deleteButton}
-                  onClick={() => deleteUser(u._id)}
-                >
-                  Delete
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </section>
-  
 
-      <section className={styles.panel}>
-        <h3 className={styles.panelTitle}>Events</h3>
-        <input
-          type="text"
-          placeholder="Search events by title"
-          value={findEvents}
-          onChange={(e) => setFindEvents(e.target.value)}
-          className={styles.searchInput}
-        />
-  
-        <ul className={styles.eventList}>
-          {filteredEvents.map((event) => (
-            <li key={event._id} className={styles.eventListItem}>
-              {editingEventId === event._id ? (
-                <>
-                  <input
-                    type="text"
-                    value={eventData.title}
-                    onChange={(e) =>
-                      setEventData({ ...eventData, title: e.target.value })
+      <div className="sections-container w-60" style={{ display: "flex" }}>
+        <section className={styles.panel}>
+          <h3 className={styles.panelTitle}>Users</h3>
+          <input
+            type="text"
+            placeholder="Search users"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className={styles.searchInput}
+          />
+
+          <ul style={{ padding: "10px 20px" }} className={styles.userList}>
+            {filteredUsers.map((u) => (
+              <li key={u._id} className={styles.userListItem}>
+                <div className={styles.userInfo}>
+                  <span className={styles.userName}>{u.firstName}</span>
+                  <span className={styles.userEmail}>({u.email})</span>
+                </div>
+
+                <div className={styles.userAction}>
+                  <button
+                    className={
+                      u.isBlocked ? styles.btnUnblock : styles.btnBlock
                     }
-                    className={styles.editInput}
-                  />
-                  <textarea
-                    value={eventData.description}
-                    onChange={(e) =>
-                      setEventData({
-                        ...eventData,
-                        description: e.target.value,
-                      })
-                    }
-                    className={styles.editTextarea}
-                  />
-                  <div className={styles.eventAction}>
-                    <button className={styles.saveButton} onClick={saveEdit}>
-                      Save
-                    </button>
-                    <button className={styles.cancelButton} onClick={cancelEditing}>
-                      Cancel
-                    </button>
-                  </div>
-                </>
-              ) : (
-                <>
-                  <div className={styles.eventInfo}>
-                    <strong>{event.title}</strong> - {event.description}
-                  </div>
-                  <div className={styles.eventAction}>
-                    <button className={styles.editButton} onClick={() => startEditingEvent(event)}>
-                      Edit
-                    </button>
-                    <button className={styles.deleteButton} onClick={() => deleteEvent(event._id)}>
-                      Delete
-                    </button>
-                  </div>
-                </>
-              )}
-            </li>
-          ))}
-        </ul>
-      </section>
-  
+                    onClick={() => toggleBlock(u._id, !u.isBlocked)}
+                  >
+                    {u.isBlocked ? "Unblock" : "Block"}
+                  </button>
+                  <button
+                    className={styles.deleteButton}
+                    onClick={() => deleteUser(u._id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </section>
+
+        <section className={styles.panel}>
+          <h3 className={styles.panelTitle}>Events</h3>
+          <input
+            type="text"
+            placeholder="Search events by title"
+            value={findEvents}
+            onChange={(e) => setFindEvents(e.target.value)}
+            className={styles.searchInput}
+          />
+
+          <ul style={{ padding: "10px 20px" }} className={styles.eventList}>
+            {filteredEvents.map((event) => (
+              <li key={event._id} className={styles.eventListItem}>
+                {editingEventId === event._id ? (
+                  <>
+                    <input
+                      type="text"
+                      value={eventData.title}
+                      onChange={(e) =>
+                        setEventData({ ...eventData, title: e.target.value })
+                      }
+                      className={styles.editInput}
+                    />
+                    <textarea
+                      value={eventData.description}
+                      onChange={(e) =>
+                        setEventData({
+                          ...eventData,
+                          description: e.target.value,
+                        })
+                      }
+                      className={styles.editTextarea}
+                    />
+                    <div className={styles.eventAction}>
+                      <button className={styles.saveButton} onClick={saveEdit}>
+                        Save
+                      </button>
+                      <button
+                        className={styles.cancelButton}
+                        onClick={cancelEditing}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div className={styles.eventInfo}>
+                      <strong>{event.title}</strong> - {event.description}
+                    </div>
+                    <div className={styles.eventAction}>
+                      <button
+                        className={styles.editButton}
+                        onClick={() => startEditingEvent(event)}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className={styles.deleteButton}
+                        onClick={() => deleteEvent(event._id)}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </>
+                )}
+              </li>
+            ))}
+          </ul>
+        </section>
+      </div>
+
       <button
         onClick={() => (window.location.href = "/")}
         className={styles.backButton}
@@ -266,7 +281,6 @@ function Admin() {
       </button>
     </div>
   );
-
 }
 
 export default Admin;
