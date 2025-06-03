b5df562a2e814e40215606ad88d875449aa7dc79
Ph1los0phy <lsimeon.minkov@gmail.com>
2025-06-04 01:17:55 +0300

    Drawer -> improved styling

--- Calendar/src/components/NavComponent/Drawer.jsx
+++ Calendar/src/components/NavComponent/Drawer.jsx
@@ -5,42 +5,57 @@ function DrawerComponent({ onLogout }) {
   return (
     <Drawer.Root>
       <Drawer.Trigger asChild>
-        <Button variant="outline" size="xs">
-          Side menu
+        <Button variant="outline" size="sm" fontSize="17px" padding={2}>
+          Menu ≡
         </Button>
       </Drawer.Trigger>
       <Portal>
         <Drawer.Backdrop />
         <Drawer.Positioner padding="4">
-          <Drawer.Content rounded="md">
+          <Drawer.Content
+            rounded="md"
+            maxH="400px"
+          >
             <Drawer.Header>
-              <Drawer.Title style={{ color: "pink" }}>Side menu</Drawer.Title>
+              <Drawer.Title alignItems={"center"} display="flex" justifyContent="center">
+                <span
+                  style={{
+                    fontSize: 20,
+                    fontWeight: "bold",
+                    color: "wheat",
+                    backgroundColor: "#242124",
+                    borderRadius: 10,
+                    padding: "3px 62px",
+                  }}
+                > User Panel </span>
+              </Drawer.Title>
             </Drawer.Header>
+            
             <Drawer.Body
               style={{
                 justifyItems: "center",
                 color: "white",
                 display: "flex",
                 flexDirection: "column",
-              }}
-            >
-              <Button variant="ghost" onClick={() => navigate("/profile")}>
-                Profile
+              }}>
+              <Button variant="ghost" fontSize={18} onClick={() => navigate("/profile")}>
+                👤 Profile
               </Button>
-              <Button variant="ghost" onClick={() => navigate("/myevents")}>
-                My Events
+              <Button variant="ghost" fontSize={18} onClick={() => navigate("/myevents")}>
+                🗓 My Events
               </Button>
             </Drawer.Body>
+
             <Drawer.Footer>
               <Button onClick={onLogout}>Logout</Button>
             </Drawer.Footer>
             <Drawer.CloseTrigger asChild>
-              <CloseButton size="sm" />
+              <CloseButton size="md" />
             </Drawer.CloseTrigger>
           </Drawer.Content>
         </Drawer.Positioner>
       </Portal>
-    </Drawer.Root>
+    </Drawer.Root >
   );
 }
 export default DrawerComponent;
