4a08c6b6896a796f2589f37d9ecdca9416f5809a
Masterclass777 <borislav.2404g@gmail.com>
2025-06-04 00:18:04 +0300

    connected front end with backend keys to be able to read make crud orders

--- Calendar/server/mongo.js
+++ Calendar/server/mongo.js
@@ -13,7 +13,6 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
       process.env.FRONT_END_URL
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
--- Calendar/src/components/Authentication/AuthContext.jsx
+++ Calendar/src/components/Authentication/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState, useEffect } from "react";
 import axios from "axios";
 
 export const AuthContext = createContext();
-
+const key = import.meta.env.VITE_FRONT_END_URL
 export default function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
@@ -18,7 +18,7 @@ export default function AuthProvider({ children }) {
   }, []);
 
   const login = async (email, password) => {
-    const res = await axios.post("http://localhost:5000/api/auth/login", {
+    const res = await axios.post(`${key}` || "http://localhost:5000/api/auth/login", {
       email,
       password,
     });
