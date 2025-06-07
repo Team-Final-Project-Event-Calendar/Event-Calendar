5d44508e01910161ab2a9677a874bd84de85caba
Tsvetanmarkov <markowcvetan@gmail.com>
2025-06-07 22:43:07 +0300

    make it work but not updating in the database edit profile details

--- Calendar/server/verify-token.js
+++ Calendar/server/verify-token.js
@@ -12,20 +12,18 @@ export default function verifyToken(req, res, next) {
   });
 }
 
-
-
 export function verifyAdmin(req, res, next) {
-  const token = req.headers.authorization?.split(' ')[1];
-  if (!token) return res.status(401).json({ message: 'Access denied' });
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Access denied" });
 
   try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      if (decoded.role !== 'admin') {
-          return res.status(403).json({ message: 'Admins only' });
-      }
-      req.user = decoded;
-      next();
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (decoded.role !== "admin") {
+      return res.status(403).json({ message: "Admins only" });
+    }
+    req.user = decoded;
+    next();
   } catch (err) {
-      return res.status(400).json({ message: 'Invalid token' });
+    return res.status(400).json({ message: "Invalid token" });
   }
 }
