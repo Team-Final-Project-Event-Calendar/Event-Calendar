28f7fbec0ba46332c383735de15b3c4dac0db4df
Tsvetanmarkov <markowcvetan@gmail.com>
2025-06-08 09:41:12 +0300

    need to sign out and sign back in to see the  changes to the image uploader

--- Calendar/src/components/NavComponent/AvatarCompont.jsx
+++ Calendar/src/components/NavComponent/AvatarCompont.jsx
@@ -1,10 +1,20 @@
 import { Avatar } from "@chakra-ui/react";
+import { useContext } from "react";
+import { AuthContext } from "../Authentication/AuthContext";
 
 function AvatarComponent() {
+  const { user } = useContext(AuthContext);
+
   return (
     <Avatar.Root>
-      <Avatar.Fallback name="Segun Adebayo" />
-      <Avatar.Image src="https://bit.ly/sage-adebayo" />
+      <Avatar.Fallback name={user ? user.username : "name"} />
+      <Avatar.Image
+        src={
+          user
+            ? user.avatar
+            : "https://t4.ftcdn.net/jpg/08/23/95/89/360_F_823958944_1c9covIC7Tl7eyJtWoTiXc0L4vP6f43q.jpg"
+        }
+      />
     </Avatar.Root>
   );
 }
