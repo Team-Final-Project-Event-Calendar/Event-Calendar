ab37a6970eaac99f07787bbc372ffa282a49ee98
Tsvetanmarkov <145834423+Markowcvetan@users.noreply.github.com>
2025-06-12 10:13:43 +0300

    add Link to profile via image in nav menu

--- Calendar/src/components/NavComponent/AvatarCompont.jsx
+++ Calendar/src/components/NavComponent/AvatarCompont.jsx
@@ -1,21 +1,23 @@
 import { Avatar } from "@chakra-ui/react";
 import { useContext } from "react";
 import { AuthContext } from "../Authentication/AuthContext";
+import { Navigate, Link } from "react-router-dom";
 
 function AvatarComponent() {
   const { user } = useContext(AuthContext);
-
   return (
-    <Avatar.Root>
-      <Avatar.Fallback name={user ? user.username : "name"} />
-      <Avatar.Image
-        src={
-          user
-            ? user.avatar
-            : "https://t4.ftcdn.net/jpg/08/23/95/89/360_F_823958944_1c9covIC7Tl7eyJtWoTiXc0L4vP6f43q.jpg"
-        }
-      />
-    </Avatar.Root>
+    <Link to="profile" style={{ paddingRight: "10px" }}>
+      <Avatar.Root>
+        <Avatar.Fallback name={user ? user.username : "name"} />
+        <Avatar.Image
+          src={
+            user
+              ? user.avatar
+              : "https://t4.ftcdn.net/jpg/08/23/95/89/360_F_823958944_1c9covIC7Tl7eyJtWoTiXc0L4vP6f43q.jpg"
+          }
+        />
+      </Avatar.Root>
+    </Link>
   );
 }
 
