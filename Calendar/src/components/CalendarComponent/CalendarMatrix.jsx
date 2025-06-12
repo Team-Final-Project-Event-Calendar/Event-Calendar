6d145f2e0d2e4f3fec3c99118b85937fcf636153
Masterclass777 <borislav.2404g@gmail.com>
2025-06-12 14:05:23 +0300

    fix the issue with interval

--- Calendar/src/components/CalendarComponent/CalendarMatrix.jsx
+++ Calendar/src/components/CalendarComponent/CalendarMatrix.jsx
@@ -20,9 +20,10 @@ function CalendarMatrix({ currentDate, view, onDayClick }) {
         }
         const repeatedEvents = [];
     
-        for (let i = 1; i <= repeatCount; i++) {  
+        for (let i = 1; i <= repeatCount; i++) {
+        
             const newDate = new Date(baseDate);
-    
+        
             if (repeatType === 'daily') {
                 newDate.setDate(newDate.getDate() + i * interval);
             } else if (repeatType === 'weekly') {
@@ -30,15 +31,16 @@ function CalendarMatrix({ currentDate, view, onDayClick }) {
             } else if (repeatType === 'monthly') {
                 newDate.setMonth(newDate.getMonth() + i * interval);
             }
-    
+        
             repeatedEvents.push({
                 ...event,
                 startDateTime: newDate.toISOString(),
-                isRepeatedInstance: i > 0,  
+                isRepeatedInstance: true,
             });
         }
+        
     
-        return repeatedEvents;
+        return [event, ...repeatedEvents];
     };
     
 
