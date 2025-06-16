4e19963221be290a777b7fa6b31a3c45fb79989c
Tsvetanmarkov <145834423+Markowcvetan@users.noreply.github.com>
2025-06-16 10:29:24 +0300

    fixing profile type to show "user" instead joined

--- Calendar/src/components/UserProfile/UserProfile.jsx
+++ Calendar/src/components/UserProfile/UserProfile.jsx
@@ -165,7 +165,7 @@ function UserProfile() {
             <strong>Phone:</strong> {userData.phoneNumber || "N/A"}
           </div>
           <div style={{ marginBottom: "12px", color: subTextColor }}>
-            <strong>Joined:</strong> {userData.role.toUpperCase()}
+            <strong>Profile:</strong> {userData.role.toUpperCase()}
           </div>
         </div>
       </div>
